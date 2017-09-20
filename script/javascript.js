e2db75cc77bceaf1a2ff8c65d063fb26d4367cb5
Jay Patel <pateljay19991@gmail.com>
2017-09-20 11:20:55 -0700

    initial commit
    changes

---
+++ script/javascript.js
@@ -0,0 +1,47 @@
+//Contact Us Page Bootstrap
+$(document).ready(function(){ 
+    $('#characterLeft').text('140 characters left');
+    $('#message').keydown(function () {
+        var max = 140;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });    
+});
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
---
+++ script/jquery.slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	$('#auto').bbslider({
+		auto: true,
+		timer:3000,
+		controls: true,
+		loop:true,
+		pauseOnHit:false
+		});
+
+	});
